ae1ab71e5c47238b3cf27cb67a653ef83e02a57d
Allenxuxu <120582243@qq.com>
2020-12-05 16:34:21 +0800

    optimize server

--- server/mucp/mucp.go
+++ server/mucp/mucp.go
@@ -5,10 +5,6 @@ import (
 	"github.com/stack-labs/stack-rpc/server"
 )
 
-var (
-	DefaultRouter = newRpcRouter()
-)
-
 // NewServer returns a stack server interface
 func NewServer(opts ...server.Option) server.Server {
 	return newRpcServer(opts...)
